29c568b9cb6dde1c4498ada3965632efd6b77917
Carolina Souza <62221034+carolsouzamuniz@users.noreply.github.com>
2020-04-23 15:13:58 -0300

    estruturação do componente filtro

--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -1,20 +1,45 @@
 import React from 'react'
 
-class Filtro extends React.Component{
-    render(){
+const CampoDeBusca = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    width: 200px;
+    padding: 10px;
+    align-items: stretch;
+`
 
+class Filtro extends React.Component{
+    state={
+
+        valorInputMinimo: '',
+        valorInputMaximo: '',
+        valorInputBusca: '',
+
+    }
+    
+    render(){
+        
         return(
             
-            <div>
-                <h3>Filtro</h3>
-                <p>Valor mínimo</p>
-                <input/>
-                <p>Valor máximo</p>
-                <input/>
-                <p>Buscar produto</p>
-                <input/>
+            <CampoDeBusca>
+                <h3>Filtros:</h3>
+                    <div>
+                        <label for="valorMinimo">Valor mínimo: </label>
+                        <input type="number" name="valorMinimo"/>
+                    </div>
+                   
+                    <div>   
+                        <label for="valorMaximo">Valor máximo: </label>
+                        <input type="number" name="valorMaximo"/>
+                    </div>
+                    
+                    <div>
+                        <label>Buscar produto: </label>
+                        <input type="text" name="busca"/>
+                    </div>
 
-            </div>
+            </CampoDeBusca>
         )
     }
 
