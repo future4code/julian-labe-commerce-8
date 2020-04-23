b3da07f1b80bdefda342e26ee0d08bd93f1d6804
Jerry Jones <jerryjfernandes@gmail.com>
2020-04-23 15:10:28 -0300

    Produtos: componente atualizado.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import Filtro from './components/Filtro'
-import Produtos from './components/Produtos'
+import Filtro from './components/Filtro';
+import Produtos from './components/Produtos';
+import styled from 'styled-components';
+
+const Principal = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`
+
+const FiltroContainer = styled.div`
+  width: 20%;
+  border: 1px solid black;
+`
+const ProdutosContainer = styled.div`
+  width: 80%;
+`
 
 
 class App extends React.Component {
   render() {
 
     return (
-      <div>
+      <Principal>
+
+        <FiltroContainer>
         <Filtro/>
+        </FiltroContainer>
+
+        <ProdutosContainer>
         <Produtos/>
+        </ProdutosContainer>
   
-      </div>
+      </Principal>
     );
   
   }
