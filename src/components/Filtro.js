07cbb5d2bf89577f64a13a15a4e9920eff2ea864
Carolina Souza <62221034+carolsouzamuniz@users.noreply.github.com>
2020-04-23 10:57:20 -0300

    Estrutura base para iniciar o projeto

---
+++ src/components/Filtro.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class Filtro extends React.Component{
+    render(){
+
+        return(
+            
+            <div>
+                <h3>Filtro</h3>
+                <p>Valor mínimo</p>
+                <input/>
+                <p>Valor máximo</p>
+                <input/>
+                <p>Buscar produto</p>
+                <input/>
+
+            </div>
+        )
+    }
+
+}
+
+export default Filtro;
